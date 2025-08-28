6a1e113c176b9b609375f47f570556beda0a1bf3
Md. Ashikul Islam <19ashikulislam@gmail.com>
2025-08-29 02:34:13 +0600

    Call functionalities added

--- script.js
+++ script.js
@@ -9,3 +9,56 @@ const heartButtons = document.getElementsByClassName('heart-btn');
     document.getElementById('heart-count').innerText = currentHeart;
 })
  }
+
+
+ 
+// Call button functionalities 
+const callBtns = document.getElementsByClassName('call-btn');
+
+for (let callButton of callBtns) {
+    callButton.addEventListener('click', function () {
+        
+        const contactCard = callButton.closest('.contact-card');
+
+        const callTitle = contactCard.querySelector('.contact-title').innerText;
+        const callNumber = contactCard.querySelector('.contact-number').innerText;
+
+        const coinElement = document.getElementById('coin-balance');
+        let currentCoins = parseInt(coinElement.innerText);
+
+        if (currentCoins >= 20) {
+            alert(`Calling ${callTitle} at ${callNumber}...`);
+
+            const newBalance = currentCoins - 20;
+            coinElement.innerText = newBalance;
+
+            const currentTime = new Date().toLocaleTimeString();
+            const historyClear = document.getElementById('call-history');
+
+            const newCard = document.createElement("div");
+            newCard.innerHTML = `
+                <div class="w-11/12 px-1 md:px-2 mx-auto mt-2 bg-[#F2F2F2] rounded-xl flex justify-between items-center flex-col md:flex-row py-2">
+                    <div class="">
+                        <h2 class="text-base md:text-xl font-bold">${callTitle}</h2>
+                        <p class="py-2 text-[#5C5C5C]">${callNumber}</p>
+                    </div>
+                    <div>
+                        <p class="text-sm text-[#5C5C5C]">${currentTime}</p>
+                    </div>
+                </div>
+            `;
+
+            historyClear.append(newCard);
+        } else {
+            alert("🚨You do not have enough coins. At least 20 coins are required to make a call.");
+            return;
+        }
+    });
+}
+
+ 
+// Call History Clear
+document.getElementById('btn-clear').addEventListener('click', function () {
+    const callHistoryClear = document.getElementById('call-history');
+    callHistoryClear.innerHTML = "";
+})
