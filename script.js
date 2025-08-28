d339c0c89fcb26ad4016de45ffee49da0c7cbd27
Md. Ashikul Islam <19ashikulislam@gmail.com>
2025-08-29 01:48:33 +0600

    Added heart number increase function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Heart Button Increase
+const heartButtons = document.getElementsByClassName('heart-btn');
 
+ for(let heartButton of heartButtons) {
+    heartButton.addEventListener('click', function(){
+    const heartCount = parseInt(document.getElementById('heart-count').innerText);
+    const currentHeart = heartCount + 1;
+
+    document.getElementById('heart-count').innerText = currentHeart;
+})
+ }
